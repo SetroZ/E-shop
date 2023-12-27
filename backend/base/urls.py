64ba06d6ace6d05b6c4742facc8f85084294347d
SetroZ <yousfsbry66@gmail.com>
2023-12-27 23:18:11 +0800

    changed products to fetching using axios

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.getRoutes, name="routes")]
+urlpatterns = [
+    path("", views.getRoutes, name="routes"),
+    path("products", views.getProducts, name="products"),
+    path("products/<str:pk>",views.getProduct,name="product")
+]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+@api_view(["GET"])
+
+# Create your views here
 def getRoutes(request):
-    return JsonResponse('Hello',safe=False)
+    return Response("Hello")
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    for product in products:
+        if product['_id'] ==pk:
+            return Response(product)

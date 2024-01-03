bebf31f635247204fe25cb558a4d08bbdb2a7f20
SetroZ <yousfsbry66@gmail.com>
2024-01-03 13:22:18 +0800

    used database instead of products file

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
 
-
-
+from .serializers import ProductSerializer
 @api_view(["GET"])
 
 # Create your views here
@@ -14,11 +14,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    for product in products:
-        if product['_id'] ==pk:
-            return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
